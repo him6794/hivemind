7112f70440419ab12d9b18c7cf2e9c42c1e3fa43
justin <jack0916295614@gmail.com>
2026-07-16 06:41:00 +0000

    chore: remove generated dependencies and obsolete subagents

--- subagents/sandbox-egress/target/debug/deps/sandbox_egress-9c14f1767d3eee5a.d
+++
@@ -1,5 +0,0 @@
-D:\hivemind\subagents\sandbox-egress\target\debug\deps\sandbox_egress-9c14f1767d3eee5a.d: src\main.rs
-
-D:\hivemind\subagents\sandbox-egress\target\debug\deps\sandbox_egress-9c14f1767d3eee5a.exe: src\main.rs
-
-src\main.rs:
--- subagents/sandbox-egress/target/debug/deps/tracing_log-49c37f5e39cb7f3a.d
+++
@@ -1,8 +0,0 @@
-D:\hivemind\subagents\sandbox-egress\target\debug\deps\tracing_log-49c37f5e39cb7f3a.d: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\lib.rs C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\log_tracer.rs
-
-D:\hivemind\subagents\sandbox-egress\target\debug\deps\libtracing_log-49c37f5e39cb7f3a.rlib: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\lib.rs C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\log_tracer.rs
-
-D:\hivemind\subagents\sandbox-egress\target\debug\deps\libtracing_log-49c37f5e39cb7f3a.rmeta: C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\lib.rs C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\log_tracer.rs
-
-C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\lib.rs:
-C:\Users\user\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-log-0.2.0\src\log_tracer.rs:
