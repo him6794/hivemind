002d50a9bd5ca647f30cf881ca62a5410c5044e6
him6794 <him6794@users.noreply.github.com>
2026-08-15 19:16:17 +0800

    fix(proto): rebuild generated bindings when schemas change

--- hivemind-rs/crates/proto/build.rs
+++ hivemind-rs/crates/proto/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../../../proto/hivemind.proto");
+    println!("cargo:rerun-if-changed=../../../proto/vpn.proto");
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
